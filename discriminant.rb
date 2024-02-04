1fab16618aa6ccc67de1b891d9dab91c81b97b64
danielbaltean <baltean222@mail.ru>
2024-02-04 18:29:44 +0200

    added 4 tasks

---
+++ discriminant.rb
@@ -0,0 +1,27 @@
+puts "Enter a"
+a = gets.to_f
+
+puts "Enter b"
+b = gets.to_f
+
+puts "Enter c"
+c = gets.to_f
+
+D = b**2 - 4 * a * c 
+
+if D > 0 
+square = Math.sqrt(D)
+x1 = (-b + square) / 2c
+x2 = (b + square) / 2c
+puts "Enter discriminant #{D}"
+puts "Enter 2 square x1 , x2 
+end
+elsif D == 0 
+x1= (-b + square) / 2c
+puts "Enter discriminant #{D}"
+puts "Enter 1 x"
+
+else D < 0
+puts "Enter discriminant, without square"
+
+end.
---
+++ formula_of_Pitagora.rb
@@ -0,0 +1,29 @@
+puts "Enter the side_a"
+side(a) = gets.to_f
+
+puts "Enter the side_b"
+side_b = gets.to_f
+
+puts "Enter the side_c"
+side_c = gets.to_f
+
+
+if a**2 = b**2 + c**2
+puts triagle has 90 degree
+else a**2 != b**2 + c**2
+puts triagle is hasn't 90 degree 
+
+end.
+
+
+
+
+
+
+
+
+
+
+
+
+
