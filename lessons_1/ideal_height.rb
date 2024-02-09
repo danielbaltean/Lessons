028e4f4bbad1bbcd0a9bd106a535cd9b0046a565
danielbaltean <baltean222@mail.ru>
2024-02-09 20:37:59 +0200

    lessons_1

--- lessons_1/ideal_height.rb
+++ lessons_1/ideal_height.rb
@@ -1,9 +1,7 @@
 puts "Name user"
 name = gets.chomp
-''
 puts "Height user"
-height = gets.to_i
-
-ideal_weight = (<height> - 110) * 1.15
-
-end.
+height = gets.chomp.to_i
+ideal_weight = (height - 110) * 1.15
+puts "#{name} #{ideal_weight}" 
+end
