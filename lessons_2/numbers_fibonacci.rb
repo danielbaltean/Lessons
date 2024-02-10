6e02114a6fb11ac68d926fa18a8e2d158e579873
danielbaltean <baltean222@mail.ru>
2024-02-10 17:34:56 +0200

    lessons_2

---
+++ lessons_2/numbers_fibonacci.rb
@@ -0,0 +1,9 @@
+numbers_fibonacci = [0,1]
+
+while numbers_fibonacci.last + numbers_fibonacci[-2]< 100 do
+next_number = numbers_fibonacci[-1] + numbers_fibonacci[-2]
+ number_fibonacci << next_number 
+ end
+
+ puts number_fibonacci
+ 
