a631211810c28ef09d3b0faac9378a1640581d77
danielbaltean <baltean222@mail.ru>
2024-02-11 17:46:38 +0200

    lessons_2

--- lessons_2/bissextile_year.rb
+++ lessons_2/bissextile_year.rb
@@ -10,7 +10,8 @@ year = gets.to_i
 amount_days_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 amount_days_month = 29 if (year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0
 
-number_month = -1
+number_month = month - 1
+amount_days_month[1] = amount_days.take 
 amount_days_year = amount_days.sum + day
 
 puts amount_days_year
