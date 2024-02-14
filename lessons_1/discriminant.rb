0a66bffeda9a9cbc032d447d459feda90a3cc8cd
danielbaltean <baltean222@mail.ru>
2024-02-14 15:13:15 +0200

    lessons_1

--- lessons_1/discriminant.rb
+++ lessons_1/discriminant.rb
@@ -7,18 +7,18 @@ b = gets.to_f
 puts "Enter c"
 c = gets.to_f
 
-D = b**2 - 4 * a * c 
+d = b**2 - 4 * a * c 
 
-if D > 0 
-  square = Math.sqrt(D)
-  x1 = (-b + square) / 2c
-  x2 = (b + square) / 2c
+if d > 0 
+  square(d) = Math.sqrt(d)
+  x1 = (-b + square(d)) / 2c
+  x2 = (b + square(d)) / 2c
   puts "Enter discriminant #{D}"
-  puts "Enter 2 square x1 , x2" 
-elsif D == 0 
-  x1= (-b + square) / 2c
+  puts "Enter 2 square #{x1} , #{x2}" 
+elsif d == 0 
+  x1= (-b + square(d)) / 2c
   puts "Enter discriminant #{D}"
-  puts "Enter 1 x"
-else D < 0
+  puts "Enter #{2x}"
+else d < 0
   puts "Enter discriminant, without square"
 end
