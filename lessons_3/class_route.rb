30e276aa25117e04db3a2c40d025bbe8ed72cb83
danielbaltean <baltean222@mail.ru>
2024-02-14 17:17:19 +0200

    lessons_3

--- lessons_3/class_route.rb
+++ lessons_3/class_route.rb
@@ -1,15 +1,16 @@
-class Route
-	attr_reader :stations
+class route
+	attr_reader stations;
 
   def initialize (begin_station, end_station)
 	@stations = [begin_station, end_station]
   end
+  
 
   def add_intermediate_station (name_station)
-	@stations_insert (-2,name_station)
+	@stations_insert = (#"-2", "name_station")
   end
 
-  def del_intermediate_station (name_station)
+  def del_intermediate_station : (name_station)
 	@station_delete (name_station) unless [@station.first, @stations.last].include?(name_station)
   end
 
